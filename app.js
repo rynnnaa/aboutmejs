0f60031373a708acb8913a1d3ac4706e1ab85718
Ryna <pinktulips@Hotmail.com>
2019-07-08 14:30:19 -0700

    made more questions and added styling

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 'use strict';
 
-alert('Welcome! Lets get to know you!');
-var userName = prompt('What is your name?');
-alert('Nice to meet you, ' + userName);
-console.log('username', userName);
-var occupation = prompt('What is it that you do for a living?');
-alert('Ok, ' + userName + '.' + ' Must be nice to be a ' + occupation + '.');
-console.log('occupation', occupation);
-var funStuff = prompt('What do you do for fun?');
-alert('Ok, ' + userName + ' who is a ' + occupation + ' .' + funStuff + ' sounds like so much fun!');
-console.log('fun stuff', funStuff);
-var food = prompt('What do you like to eat?');
-alert('Ew, ' + food + ' is gross. Very nice to meet you ' + userName + ' who is a ' + occupation + ' that likes to ' + funStuff);
-console.log('food', food);
+alert('Hello. Welcome to my Yes/No Game. Please answer yes or no.');
+var answerOne = prompt('Is global warming real?').toLowerCase();
+console.log('global warming', answerOne);
+alert('Global warming is real and I respect your opinion.');
+var answerTwo = prompt('Is it possible to lick your elbow?').toLowerCase();
+console.log('elbow', answerTwo);
+alert('It is possuble, and I respect your opinion');
+var answerThree = prompt('Is Christmas the best holiday?').toLowerCase();
+console.log('christmas', answerThree);
+alert('That\'s your opinion');
+var answerFour = prompt('Is water consumption necessary?').toLowerCase();
+console.log('water', answerFour);
+alert('Water consumption is definitely necessary and that\'s my opinion');
+var answerFive = prompt('Do people have gills?').toLowerCase();
+console.log('gills', answerFive);
+alert('No, but that\'s your opinion');
+
+
+
+
